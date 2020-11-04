9d359cf7b10fae68c71493500bb56f19eee6ef02
makhtar.wade <makhtar.wade@m2t.biz>
2020-11-04 22:53:26 +0000

    mediaquery matchers

--- backoffice/src/environments/environment.prod.ts
+++ backoffice/src/environments/environment.prod.ts
@@ -2,11 +2,11 @@ import {KeycloakConfig} from 'keycloak-js';
 
 const keycloakConfig: KeycloakConfig = {
   realm: 'atps',
-  url: 'http://192.168.215.1:9007/auth',
+  url: 'http://192.168.1.37:9007/auth',
   clientId: 'mobilepaiement'
 };
 export const environment = {
   production: true,
-  baseUrl: 'http://localhost:9002',
+  baseUrl: 'http://192.168.1.37:9002',
   key: keycloakConfig
 };
