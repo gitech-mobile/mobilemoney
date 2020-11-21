af9655d72aa5dbda2c2a4b8d6971dc8430feb53f
makhtar.wade <makhtar.wade@m2t.biz>
2020-11-21 17:12:57 +0000

    preprod ok

--- backoffice/src/environments/environment.prod.ts
+++ backoffice/src/environments/environment.prod.ts
@@ -2,11 +2,11 @@ import {KeycloakConfig} from 'keycloak-js';
 
 const keycloakConfig: KeycloakConfig = {
   realm: 'mobilemoney',
-  url: 'http://192.168.1.4:9007/auth',
+  url: 'http://192.168.1.6:9007/auth',
   clientId: 'publicmobilepaiement'
 };
 export const environment = {
   production: true,
-  baseUrl: 'http://192.168.1.4:9002',
+  baseUrl: 'http://192.168.1.6:9002',
   key: keycloakConfig
 };
